4b3553e8376d980fea38a54283d62870f4d4a34b
lucKulke <lucaskulke98@gmail.com>
2023-09-22 16:09:27 +0200

    implemented recording function

--- src/components/RecordingButton.jsx
+++ src/components/RecordingButton.jsx
@@ -1,8 +1,16 @@
 import { MdOutlineRecordVoiceOver } from "react-icons/md";
-function RecordingButton() {
+function RecordingButton(props) {
   return (
     <div className="w-full flex justify-center mt-10 mb-10 items-center">
-      <button className="bg-yellow-500 hover:bg-yellow-400 rounded-full h-20 w-20 flex items-center justify-center active:animate-pulse active:ring-8 active:ring-white active:ring-of">
+      <button
+        onMouseDown={props.onMouseDown}
+        onMouseUp={props.onMouseUp}
+        onMouseLeave={props.onMouseLeave} // Handle release when the mouse leaves the button
+        onTouchStart={props.onTouchStart} // Handle touch events on mobile devices
+        onTouchEnd={props.onTouchEnd}
+        disabled={props.disabled}
+        className="bg-yellow-500 hover:bg-yellow-400 disabled:bg-gray-400 rounded-full h-20 w-20 flex items-center justify-center active:animate-pulse active:ring-8 active:ring-white active:ring-of"
+      >
         <MdOutlineRecordVoiceOver className="h-8 w-8" />
       </button>
     </div>
--- src/sections/Conversation.jsx
+++ src/sections/Conversation.jsx
@@ -11,17 +11,67 @@ function Conversation() {
   const [processing, setProcessing] = useState(false);
   const [aiSpeaking, setAiSpeaking] = useState(false);
 
+  const [recordingStoped, setRecordingStoped] = useState(false);
+
+  const [mediaStream, setMediaStream] = useState(null);
+  const [mediaRecorder, setMediaRecorder] = useState(null);
+  const [audioChunks, setAudioChunks] = useState([]);
+
   const [historyVisibility, setHistoryVisibility] = useState(false);
 
   const handleHistoryButton = () => {
     setHistoryVisibility((prev) => !prev);
   };
 
+  const startRecording = async () => {
+    console.log("startRecording function");
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      setMediaStream(stream);
+
+      const recorder = new MediaRecorder(stream);
+      setMediaRecorder(recorder);
+
+      recorder.ondataavailable = (e) => {
+        if (e.data.size > 0) {
+          setAudioChunks([...audioChunks, e.data]);
+        }
+      };
+
+      recorder.onstop = () => {
+        const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
+        const audioUrl = URL.createObjectURL(audioBlob);
+        console.log("Audio URL:", audioUrl);
+        setAudioChunks([]);
+      };
+
+      recorder.start();
+      setRecording(true);
+    } catch (error) {
+      console.error("Error starting recording:", error);
+    }
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorder && recording) {
+      console.log("stopRecording function");
+      mediaRecorder.stop();
+      setRecordingStoped(true);
+    }
+  };
+
   return (
     <div className="max-container">
       <Steps step1={recording} step2={processing} step3={aiSpeaking} />
       <InputOutputFields />
-      <RecordingButton />
+      <RecordingButton
+        onMouseDown={startRecording}
+        onMouseUp={stopRecording}
+        onMouseLeave={stopRecording} // Handle release when the mouse leaves the button
+        onTouchStart={startRecording} // Handle touch events on mobile devices
+        onTouchEnd={stopRecording}
+        disabled={recordingStoped}
+      />
       <div className="max-md:hidden">
         <ChatHistory />
       </div>
