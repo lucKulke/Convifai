f8f6d562c2cd28c464eaaa822002c8b2d23c34a4
lucKulke <lucaskulke98@gmail.com>
2023-09-24 20:23:55 +0200

    changed style from action buttons on card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,15 +14,21 @@ export default function Card(props) {
           <h2 className="card-title">{props.language}</h2>
           <p className="mb-10">{props.title}</p>
           <div className="flex justify-between">
-            <button>
-              <ImEnter className="h-6 w-6" />
-            </button>
-            <button>
-              <HiOutlineRefresh className="h-6 w-6" />
-            </button>
-            <button onClick={() => props.delete(props.id)}>
-              <AiFillDelete className="h-6 w-6" />
-            </button>
+            <div className="w-1/3  h-10 flex justify-start">
+              <button>
+                <ImEnter className="h-7 w-7 hover:w-8 hover:h-8" />
+              </button>
+            </div>
+            <div className="w-1/3 h-10 flex justify-center">
+              <button>
+                <HiOutlineRefresh className="h-7 w-7 hover:w-8 hover:h-8" />
+              </button>
+            </div>
+            <div className="w-1/3 h-10 flex justify-end">
+              <button onClick={() => props.delete(props.id)}>
+                <AiFillDelete className="h-7 w-7 hover:w-8 hover:h-8" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
