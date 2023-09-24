0a6a23dc0800ed5c2e74cd4229d6401044e98d6f
lucKulke <lucaskulke98@gmail.com>
2023-09-24 20:20:40 +0200

    removed tw-elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   plugins: [require("daisyui")],
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     extend: {
       screens: {
