ba911c1facee87976e57f76149e0e71c4378abd7
lucKulke <lucaskulke98@gmail.com>
2023-10-11 20:09:21 +0200

    created voice animation

---
+++ front-end/src/components/VoiceAnimation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function VoiceAnimation() {
+  return (
+    <div className="flex justify-center h-[300px]">
+      <div id="bars">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
+    </div>
+  );
+}
+
+export default VoiceAnimation;
