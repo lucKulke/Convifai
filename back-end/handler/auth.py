a4e7417f95de29a3865555dea3344a9326c74965
lucKulke <lucaskulke98@gmail.com>
2023-10-13 17:37:30 +0200

    escaped preflight (cors)

--- back-end/handler/auth.py
+++ back-end/handler/auth.py
@@ -17,56 +17,64 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=["POST"])
 def login():
-    data = request.get_json()
-    print(data, flush=True)
-    username = data.get("username")
-    password = data.get("password")
-    print(password, flush=True)
+    if request.method == "POST":
+        data = request.get_json()
+        print(data, flush=True)
+        username = data.get("username")
+        password = data.get("password")
+        print(password, flush=True)
 
-    user = User.query.filter_by(username=username).first()
-    if user:
-        if check_password_hash(user.password, password):
-            login_user(user, remember=True)
-            return Response(status=201)
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user, remember=True)
+                return Response(status=201)
+            else:
+                return Response("wrong password", status=423)
         else:
-            return Response("wrong password", status=423)
-    else:
-        return Response("no such entry", status=406)
+            return Response("no such entry", status=406)
+    return "no post method"
 
 
 @auth.route("/login_status")
 def login_status():
-    if current_user.is_authenticated:
-        return Response("true", status=200)
-    else:
-        return Response("false", status=204)
+    if request.method == "GET":
+        if current_user.is_authenticated:
+            return Response("true", status=200)
+        else:
+            return Response("false", status=204)
+    return "no get method"
 
 
 @auth.route("/logout", methods=["POST"])
 @login_required
 def logout():
-    logout_user()
+    if request.method == "POST":
+        logout_user()
 
-    return Response("logged out", status=201)
+        return Response("logged out", status=201)
+    return "no post method"
 
 
 @auth.route("/sign_up", methods=["POST"])
 def sign_up():
-    data = request.get_json()
-    username = data.get("username")
-    password = data.get("password")
+    if request.method == "POST":
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
 
-    user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first()
 
-    if user:
-        return Response("you already have an account", status=406)
-    else:
-        new_user = User(
-            id=uuid.uuid4(),
-            username=username,
-            password=generate_password_hash(password=password, method="sha256"),
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user)
-        return Response("signed up", status=201)
+        if user:
+            return Response("you already have an account", status=406)
+        else:
+            new_user = User(
+                id=uuid.uuid4(),
+                username=username,
+                password=generate_password_hash(password=password, method="sha256"),
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return Response("signed up", status=201)
+    return "no post method"
