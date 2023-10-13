ce7a771b2a2280cbae18ec4b439c892665781e37
lucKulke <lucaskulke98@gmail.com>
2023-10-13 17:38:31 +0200

    fixed resizing bug

--- front-end/src/components/AiChatBubble.jsx
+++ front-end/src/components/AiChatBubble.jsx
@@ -2,7 +2,7 @@ import aiPicture from "../assets/images/ai_profile_pic.png";
 function AiChatBubble(props) {
   return (
     <div className="w-full flex justify-end">
-      <button className="border-2 max-w-3/4 border-gray-300 shadow-xl rounded-2xl p-3">
+      <button className="border-2 w-3/4 border-gray-300 shadow-xl rounded-2xl p-3">
         <p className="font-mono text-left">{props.text}</p>
       </button>
       <div className="w-10 h-10 rounded-full border-2 border-gray-300 shadow-xl ml-3">
