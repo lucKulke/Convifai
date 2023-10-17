5d5b5b463c6260d6706aa8e9b107d4b3bfc2e067
lucKulke <lucaskulke98@gmail.com>
2023-10-17 13:15:01 +0200

    comment for testing purpose

--- back-end/main.py
+++ back-end/main.py
@@ -3,4 +3,4 @@ from handler import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(port=8000, debug=True)  # for network test host="0.0.0.0"
