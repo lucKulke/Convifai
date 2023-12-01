1a61b3c13faed174ed83f4e2055ce9c9e3da786a
lucKulke <lucaskulke98@gmail.com>
2023-12-01 21:54:10 +0100

    prevent recording button from copy behavior

--- front-end/src/components/RecordingButton.jsx
+++ front-end/src/components/RecordingButton.jsx
@@ -1,11 +1,15 @@
 import { MdOutlineRecordVoiceOver } from "react-icons/md";
 function RecordingButton(props) {
+  const handleContextMenu = (e) => {
+    e.preventDefault(); // Prevent the context menu from appearing
+  };
   return (
-    <div className="w-full flex justify-center mt-10 mb-10 items-center">
+    <div className="w-full flex justify-center mt-2 mb-2 items-center">
       <button
         onMouseDown={props.onMouseDown}
         onMouseUp={props.onMouseUp}
-        onTouchStart={props.onTouchStart} // Handle touch events on mobile devices
+        onTouchStart={props.onTouchStart}
+        onContextMenu={handleContextMenu} // Handle touch events on mobile devices
         onTouchEnd={props.onTouchEnd}
         disabled={props.disabled}
         className="bg-yellow-500 hover:bg-yellow-400 disabled:bg-gray-400 rounded-full h-20 w-20 flex items-center justify-center active:animate-pulse active:ring-8 active:ring-white shadow-xl"
