e7d50b59c6d988827b09bf6acda6280e8fd6f821
lucKulke <lucaskulke98@gmail.com>
2023-12-01 21:56:27 +0100

    implemented voice animation and audio controls

--- front-end/src/components/VoiceAnimation.jsx
+++ front-end/src/components/VoiceAnimation.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlinePlayCircle, AiOutlinePauseCircle } from "react-icons/ai";
+import { FcCancel } from "react-icons/fc";
 
-function VoiceAnimation() {
+function VoiceAnimation(props) {
+  const [paused, setPaused] = useState(false);
+  const numberOfBars = 11; // max 11
+  const bars = [];
+  for (let i = 0; i < numberOfBars; i++) {
+    bars.push(
+      <div className="bar" style={paused ? { animation: "none" } : {}}></div>
+    );
+  }
+
+  const handlePause = () => {
+    setPaused(true);
+    props.pause_audio();
+  };
+
+  const handleStart = () => {
+    setPaused(false);
+    props.start_audio();
+  };
+
+  const handleCancel = () => {
+    props.cancel_audio();
+  };
+
   return (
-    <div className="flex justify-center h-[300px]">
-      <div id="bars">
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
-      </div>
+    <div className="flex justify-center items-center h-[150px]">
+      {!paused ? (
+        <button className="mr-20 max-sm:mr-5 " onClick={() => handlePause()}>
+          <AiOutlinePauseCircle className="h-10 w-10" />
+        </button>
+      ) : (
+        <button className="mr-20 max-sm:mr-5 " onClick={() => handleStart()}>
+          <AiOutlinePlayCircle className="h-10 w-10" />
+        </button>
+      )}
+      {bars}
+      <button className="ml-20 max-sm:ml-5" onClick={() => handleCancel()}>
+        <FcCancel className="h-10 w-10" />
+      </button>
     </div>
   );
 }
