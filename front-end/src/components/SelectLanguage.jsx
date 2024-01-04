bfdd39dd3a05ec6bf5dbefd6e26bfc755656a2ae
lucKulke <lucaskulke98@gmail.com>
2024-01-05 00:22:09 +0100

    added keys to language list

--- front-end/src/components/SelectLanguage.jsx
+++ front-end/src/components/SelectLanguage.jsx
@@ -10,7 +10,7 @@ function SelectLanguage(props) {
     <div className="overflow-y-auto h-40 w-1/2 shadow-2xl rounded-lg ">
       <ul className="text-center space-y-3 pt-2">
         {props.languages.map((language) => (
-          <li className="text-2xl font-mono">
+          <li className="text-2xl font-mono" key={language}>
             <button
               onClick={() => handleSelectLanguage(language)}
               className="hover:shadow-md hover:bg-blue-100 hover:rounded-xl hover:p-1 active:bg-blue-300 active:rounded-xl active:p-1"
