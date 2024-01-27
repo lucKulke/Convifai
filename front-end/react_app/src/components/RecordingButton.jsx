80354d9d993f4a0d9ebf84085f4fffafa9ba1a3a
Lucas <75575880+lucKulke@users.noreply.github.com>
2024-01-27 01:06:51 +0100

    different recording button for mobile devices (#21)
    * fix for recording button mark event
    
    * Revert "fix for recording button mark event"
    
    This reverts commit 5db479a90cd09535686ce7cbaf434d027a03031f.
    
    * added react-device-detect package
    
    * changed recording button for mobile devices

--- front-end/react_app/src/components/RecordingButton.jsx
+++ front-end/react_app/src/components/RecordingButton.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { MdOutlineRecordVoiceOver } from "react-icons/md";
+import { isMobile } from "react-device-detect";
 function RecordingButton(props) {
   const [permissionGranted, setPermissionGranted] = useState(false);
-
-  const handleRecordingPermissionStatus = async () => {
+  const [mobileDevice, setMobileDevice] = useState(isMobile);
+  const [recording, setRecording] = useState(false);
+  const checkRecordingPermissionStatus = async () => {
     const permissionStatus = await navigator.permissions.query({
       name: "microphone",
     });
@@ -27,8 +29,17 @@ function RecordingButton(props) {
     }
   };
 
+  const handleRecordButtonOnMobile = () => {
+    setRecording(!recording);
+    if (recording) {
+      props.onMouseUp();
+    } else {
+      props.onMouseDown();
+    }
+  };
+
   useEffect(() => {
-    handleRecordingPermissionStatus();
+    checkRecordingPermissionStatus();
   }, []);
 
   const handleContextMenu = (e) => {
@@ -42,22 +53,33 @@ function RecordingButton(props) {
   return (
     <div className="w-full flex justify-center mt-2 mb-2 items-center">
       {permissionGranted ? (
-        <button
-          onMouseDown={props.onMouseDown}
-          onMouseUp={props.onMouseUp}
-          onTouchStart={props.onTouchStart}
-          onTouchMove={onTouchMove}
-          onContextMenu={handleContextMenu} // Handle touch events on mobile devices
-          onTouchEnd={props.onTouchEnd}
-          disabled={props.disabled}
-          className="bg-yellow-500 hover:bg-yellow-400 disabled:bg-gray-400 rounded-full h-20 w-20 flex items-center justify-center active:animate-pulse active:ring-8 active:ring-white shadow-xl"
-        >
-          <MdOutlineRecordVoiceOver className="h-8 w-8" />
-        </button>
+        <>
+          {mobileDevice ? (
+            <button
+              onTouchStart={handleRecordButtonOnMobile}
+              onContextMenu={handleContextMenu} // Handle touch events on mobile devices
+              disabled={props.disabled}
+              className={`${
+                recording ? "bg-red-500 animate-pulse" : "bg-yellow-500"
+              } disabled:bg-gray-400 rounded-full h-20 w-20 flex items-center justify-center shadow-xl`}
+            >
+              <MdOutlineRecordVoiceOver className="h-8 w-8" />
+            </button>
+          ) : (
+            <button
+              onMouseDown={props.onMouseDown}
+              onMouseUp={props.onMouseUp}
+              onContextMenu={handleContextMenu} // Handle touch events on mobile devices
+              disabled={props.disabled}
+              className="bg-yellow-500 hover:bg-yellow-400 disabled:bg-gray-400 rounded-full h-20 w-20 flex items-center justify-center active:animate-pulse active:ring-8 active:bg-red-500 active:ring-white shadow-xl"
+            >
+              <MdOutlineRecordVoiceOver className="h-8 w-8" />
+            </button>
+          )}
+        </>
       ) : (
         <button
           onClick={handleRequestRecordingPermission} // Request permission on button
-          click
           disabled={permissionGranted}
           className=" border-black border-2 bg-blue-300 hover:bg-blue-200 animate-pulse
           px-4 py-2 rounded-xl"
