6a1ab2b3412e4aa12a85f32b1ff292121dfd0925
lucKulke <lucaskulke98@gmail.com>
2024-01-31 00:46:11 +0100

    added impressum

--- front-end/react_app/src/constans/index.js
+++ front-end/react_app/src/constans/index.js
@@ -2,4 +2,5 @@ export const navLinks = [
   { href: "/", label: "Home" },
   { href: "/conversations", label: "Conversations" },
   { href: "/about-me", label: "About Me / Project" },
+  { href: "/impressum", label: "Impressum" },
 ];
