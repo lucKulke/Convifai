a2187c0fe69dea6df448c5d16b96735483b7046e
lucKulke <lucaskulke98@gmail.com>
2024-01-31 03:03:17 +0100

    text overflow handling for inputoutput fields

--- front-end/react_app/src/components/InputOutputFields.jsx
+++ front-end/react_app/src/components/InputOutputFields.jsx
@@ -7,11 +7,15 @@ function InputOutputFields(props) {
     <div className="w-full flex flex-col justify-between 2xl:h-[500px] lg:h-[330px] sm:h-[310px] max-sm:h-[350px]">
       <div className="flex w-full justify-center">
         <ul className="space-y-2 max-md:w-3/4 md:w-2/3 m-5 mt-10">
-          <div className="border-2 border-gray-300 w-full p-3 rounded-xl shadow-lg min-h-[50px]">
-            <p className="userInput">{props.userInput}</p>
+          <div className="border-2 border-gray-300 flex w-full p-3 rounded-xl shadow-lg min-h-[50px] max-h-">
+            <div className="overflow-y-auto max-sm:max-h-24 sm:max-h-20 lg:max-h-24 2xl:max-h-28 ">
+              <p className="userInput">{props.userInput}</p>
+            </div>
           </div>
-          <div className="border-2 border-gray-300 w-full p-3 rounded-xl shadow-lg min-h-[50px]">
-            <p className="aiOutput">{props.aiOutput}</p>
+          <div className="border-2 flex border-gray-300 w-full p-3 rounded-xl shadow-lg min-h-[50px]">
+            <div className="overflow-y-auto max-sm:max-h-28 sm:max-h-24 lg:max-h-28 2xl:max-h-32 ">
+              <p className="aiOutput">{props.aiOutput}</p>
+            </div>
           </div>
         </ul>
       </div>
